54a2f4ec3b2aaa885c19ece41b8e4864e664fa6b
Miguel Martinazzo <miguelmartinazzo@gmail.com>
2024-03-21 21:43:22 -0300

    new project: creating a button page

---
+++ projeto-materias/src/app/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() textButton:string;
+}
