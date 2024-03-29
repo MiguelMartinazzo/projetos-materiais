107099c099fa54f82ec071b6ba5e58d24aa787c2
Miguel Martinazzo <miguelmartinazzo@gmail.com>
2024-03-28 21:38:00 -0300

    feat: login page and home page

--- projeto-materias/src/app/app.module.ts
+++ projeto-materias/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
--- projeto-materias/src/app/components/button/button.component.ts
+++ projeto-materias/src/app/components/button/button.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() textButton:string;
+  @Input() size: string
 }
